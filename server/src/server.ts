c10047840d32710bd91e6eb623985c3dd35d653e
ma. fernanda rodriguez r <mafda13@gmail.com>
2020-06-02 18:25:19 -0300

    include cors, images, path, and routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,47 +1,14 @@
 import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+app.use(routes);
 
-// rota: endereço completo da requisição
-// recurso: qual entidade estamos accesando do sistema
-
-// metodos http:
-// get: buscar uma o mais informações do back-end
-// post: criar novas informações do back-end
-// put: atualizar informações do back-end
-// delete: remover informações do back-end
-
-// Request Param: parâmetros que vem na propia rota que identificam um recurso 
-// Query Param: Parametros que vem na propria rota geralmente opcionais para filtros, paginação
-// Request Body: Parametros para criação/atualização de informações
-
-const users = [
-  'Diego',
-  'Cleiton',
-  'Robson',
-  'Daniel'
-];
-
-app.get('/users', (request, response) => {
-  console.log('Listagem de usuarios');
-
-  return response.json(users);
-});
-
-app.get('/users/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const user = users[id];
-  return response.json(user);
-});
-
-app.post('/users', (request, response) => {
-  const user = {
-    name: 'Diego',
-    email: 'diego@rocketseat.com.br'
-  };
-  return response.json(user);
-})
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
