6250e7d8ec85e61e60f3b110321b29723ed7fa83
ma. fernanda rodriguez r <mafda13@gmail.com>
2020-06-02 14:11:15 -0300

    rotas e recursos back-end

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,15 +2,46 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+// rota: endereço completo da requisição
+// recurso: qual entidade estamos accesando do sistema
+
+// metodos http:
+// get: buscar uma o mais informações do back-end
+// post: criar novas informações do back-end
+// put: atualizar informações do back-end
+// delete: remover informações do back-end
+
+// Request Param: parâmetros que vem na propia rota que identificam um recurso 
+// Query Param: Parametros que vem na propria rota geralmente opcionais para filtros, paginação
+// Request Body: Parametros para criação/atualização de informações
+
+const users = [
+  'Diego',
+  'Cleiton',
+  'Robson',
+  'Daniel'
+];
+
 app.get('/users', (request, response) => {
   console.log('Listagem de usuarios');
 
-  response.json([
-    'Diego',
-    'Cleiton',
-    'Robson',
-    'Daniel'
-  ]);
+  return response.json(users);
 });
+
+app.get('/users/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const user = users[id];
+  return response.json(user);
+});
+
+app.post('/users', (request, response) => {
+  const user = {
+    name: 'Diego',
+    email: 'diego@rocketseat.com.br'
+  };
+  return response.json(user);
+})
 
 app.listen(3333);
