22b7cbff642381a7630d55a8abdfcc5b2512dfea
ma. fernanda rodriguez r <mafda13@gmail.com>
2020-06-04 00:06:38 -0300

    criando novo cadastro

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState, ChangeEvent } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import axios from 'axios';
@@ -34,13 +34,23 @@ const CreatePoint = () => {
 
   const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0]);
 
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    whatsapp: '',
+  });
+
   const [selectedUf, setSelectedUf] = useState('0');
   const [selectedCity, setSelectedCity] = useState('0');
+  const [selectedItems, setSelectedItems] = useState<number[]>([]);
   const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+
+  const history = useHistory();
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(position => {
       const { latitude, longitude } = position.coords;
+      console.log(position.coords);
 
       setInitialPosition([latitude, longitude]);
 
@@ -93,7 +103,51 @@ const CreatePoint = () => {
     setSelectedPosition([
       event.latlng.lat,
       event.latlng.lng,
-    ])
+    ]);
+  }
+
+  function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+    const { name, value } = event.target;
+
+    setFormData({ ...formData, [name]: value });
+  }
+
+  function handleSelectItem(id: number) {
+    const alreadySelected = selectedItems.findIndex(item => item === id);
+
+    if (alreadySelected >= 0) {
+      const filteredItems = selectedItems.filter(item => item !== id);
+
+      setSelectedItems(filteredItems);
+    } else {
+      setSelectedItems([...selectedItems, id]);
+    }
+  }
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    const { name, email, whatsapp } = formData;
+    const uf = selectedUf;
+    const city = selectedCity;
+    const [latitude, longitude] = selectedPosition;
+    const items = selectedItems;
+
+    const data = {
+      name,
+      email,
+      whatsapp,
+      uf,
+      city,
+      latitude,
+      longitude,
+      items
+    };
+
+    await api.post('points', data);
+
+    alert('ponto de coleta criado');
+    history.push('/');
   }
 
 
@@ -107,7 +161,7 @@ const CreatePoint = () => {
         </Link>
       </header>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1>Cadastro do <br /> ponto de coleta</h1>
 
         <fieldset>
@@ -119,7 +173,8 @@ const CreatePoint = () => {
             <input
               type="text"
               name="name"
-              id="name" />
+              id="name"
+              onChange={handleInputChange} />
           </div>
 
           <div className="field-group">
@@ -128,14 +183,16 @@ const CreatePoint = () => {
               <input
                 type="email"
                 name="email"
-                id="email" />
+                id="email"
+                onChange={handleInputChange} />
             </div>
             <div className="field">
               <label htmlFor="whatsapp">Whatsapp</label>
               <input
                 type="text"
                 name="whatsapp"
-                id="whatsapp" />
+                id="whatsapp"
+                onChange={handleInputChange} />
             </div>
           </div>
 
@@ -186,7 +243,11 @@ const CreatePoint = () => {
 
           <ul className="items-grid">
             {items.map(item => (
-              <li key={item.id}>
+              <li
+                key={item.id}
+                onClick={() => handleSelectItem(item.id)}
+                className={selectedItems.includes(item.id) ? 'selected' : ''}
+              >
                 <img src={item.image_url} alt={item.title} />
                 <span>{item.title}</span>
               </li>
