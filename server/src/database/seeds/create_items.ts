0c1e448ea2492e01533587aa2701f98b28af46c9
ma. fernanda rodriguez r <mafda13@gmail.com>
2020-06-03 21:52:46 -0300

    atualizar nome das imagens

--- server/src/database/seeds/create_items.ts
+++ server/src/database/seeds/create_items.ts
@@ -4,8 +4,8 @@ export async function seed(knex: Knex) {
   await knex('items').insert([
     { title: 'Lâmpadas', image: 'lampadas.svg' },
     { title: 'Pilhas e Baterias', image: 'baterias.svg' },
-    { title: 'Papéis e Papelão', image: 'papeispapelao.svg' },
-    { title: 'Resíduos Electrônicos', image: 'electronicos.svg' },
+    { title: 'Papéis e Papelão', image: 'papeis-papelao.svg' },
+    { title: 'Resíduos Electrônicos', image: 'eletronicos.svg' },
     { title: 'Resíduos Orgânicos', image: 'organicos.svg' },
     { title: 'Óleo de Cozinha', image: 'oleo.svg' },
   ]);
